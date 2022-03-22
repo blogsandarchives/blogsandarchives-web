04bcfd5df6e2866192b4fae10bf181f01b00abba
Frankium <tis7bfrankie@gmail.com>
2022-03-22 15:31:54 +0800

    add title component

---
+++ components/Ba2Title.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>
+    <span class="symbol">:\\</span><span class="text">blogsandarchives</span
+    ><span class="symbol">\</span><span class="text">{{ name }}</span>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Ba2Title',
+  props: {
+    name: {
+      type: String,
+      default: 'null',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+span {
+  font-size: $typography-title;
+
+  &.symbol {
+    color: $foreground-gray;
+  }
+}
+</style>
