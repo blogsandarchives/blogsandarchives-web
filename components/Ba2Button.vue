282b6e2cf1ee6bcb49dee3df1b8ebfb4ee773a8b
Frankium <tis7bfrankie@gmail.com>
2022-03-22 14:34:01 +0800

    add custom button

---
+++ components/Ba2Button.vue
@@ -0,0 +1,73 @@
+<template>
+  <button :class="getButtonClass" @click="click">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Ba2Button',
+  props: {
+    click: {
+      type: Function,
+      default: () => {
+        alert('Empty Button')
+      },
+    },
+    text: {
+      type: Boolean,
+      default: false,
+    },
+    paddingType: {
+      type: String,
+      default: 'default',
+    },
+  },
+  computed: {
+    getButtonClass() {
+      let classes = this.text ? 'text-button' : ''
+      if (this.paddingType !== 'default') {
+        classes += ` ${this.paddingType}-padding-button`
+      }
+      return classes
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  cursor: pointer;
+  user-select: none;
+}
+
+.dark-mode {
+  button {
+    color: $foreground-white;
+    background-color: $gray;
+    border-width: 0;
+    padding: 0.5em 2em;
+
+    &:hover {
+      background-color: $focused-gray;
+    }
+
+    &.limited-padding-button {
+      padding: 4px 8px;
+      border-radius: 4px;
+    }
+
+    &.none-padding-button {
+      padding: 0px 0px;
+    }
+
+    &.text-button {
+      background-color: transparent;
+
+      &:hover {
+        background-color: $gray;
+      }
+    }
+  }
+}
+</style>
