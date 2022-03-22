dfdea0338b5fd188957df57d499249cf8c1af40b
Frankium <tis7bfrankie@gmail.com>
2022-03-22 12:48:46 +0800

    downgrade color-mode module to 2.1.1

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,6 +52,11 @@ export default {
     langDir: '~/langs/',
   },
 
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
