6b9e1a4c9ebbd57b04612ad1d37c9d042e645662
Frankium <tis7bfrankie@gmail.com>
2022-03-22 16:02:18 +0800

    add container layout component

---
+++ components/Ba2Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container"><slot /></div>
+</template>
+
+<script>
+export default {
+  name: 'Ba2Container',
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+  padding: 0 100px;
+
+  @media screen and (max-width: $bp-small) {
+    width: 750px;
+    padding: 0 50px;
+  }
+  @media screen and (min-width: $bp-medium) {
+    padding: 0 300px;
+  }
+}
+</style>
