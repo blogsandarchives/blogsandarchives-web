f8dc15f139071b33e441de904a3590dc8dd022bd
Frankium <tis7bfrankie@gmail.com>
2022-03-22 15:31:48 +0800

    add center layout component

---
+++ components/Ba2Center.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :style="getComputedStyle"><slot /></div>
+</template>
+
+<script>
+export default {
+  name: 'Ba2Center',
+  props: {
+    fullHeight: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    getComputedStyle() {
+      return {
+        height: this.fullHeight ? '100%' : '',
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  display: grid;
+  place-items: center;
+  height: 100%;
+}
+</style>
