8cec6ad71920ac7f5cebc279a850d6c09144fb18
Frankium <tis7bfrankie@gmail.com>
2022-03-22 14:44:29 +0800

    add styles to page globally

--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,39 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style lang="scss">
+@font-face {
+  font-family: 'Victor Mono';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('@/assets/fonts/VictorMono-Regular.ttf') format('truetype');
+}
+
+@font-face {
+  font-family: 'Victor Mono';
+  font-style: normal;
+  font-weight: bold;
+  font-display: swap;
+  src: url('@/assets/fonts/VictorMono-Bold.ttf') format('truetype');
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.dark-mode {
+  * {
+    font-family: 'Victor Mono', monospace;
+    color: $foreground-white;
+    font-weight: bold;
+    font-size: $typography-content;
+  }
+
+  body {
+    background-color: $background-gray;
+  }
+}
+</style>
